d833bb3e647c58b8b7e2994b5fd4016d67c2fa96
Shinichiro Yabu <scin.ichi@gmail.com>
2022-11-15 09:15:48 +0900

    イオン銀行：ログインの autocomplete 属性がおかしい問題を修正

--- src/自動.js
+++ src/自動.js
@@ -1,19 +1,32 @@
 (() => {
-	switch (location.origin) {
+	const { origin, pathname } = location
+	const qs = (query) => document.querySelector(query)
+
+	switch (origin) {
 		case 'https://secure.goldpoint.co.jp':
-			switch (location.pathname) {
+			switch (pathname) {
 				case '/gpm/membertop.html': {
-					const link = document.querySelector('a[href*="/web_meisai/"]')
+					const link = qs('a[href*="/web_meisai/"]')
 					link && link.click()
 					break
 				}
 				case '/memx/web_meisai/top/index.html': {
-					const link = document.querySelector('.btn_big_size')
+					const link = qs('.btn_big_size')
 					if (!link || !/\bCSV\b/.test(link.textContent)) break
 					link.click()
 					break
 				}
 			}
 			break
+		case 'https://www.aeon.co.jp':
+			switch (pathname) {
+				case '/auth/realms/msweb/protocol/openid-connect/auth': {
+					const input = qs('input#username')
+					if (!input) break
+					input.autocomplete = 'username'
+					input.focus()
+					break
+				}
+			}
 	}
 })()
