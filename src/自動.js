b3b05023377bced88907be6e30cd61017a7705e2
Shinichiro Yabu <scin.ichi@gmail.com>
2022-11-21 00:17:35 +0900

    Remove error handlings

--- src/自動.js
+++ src/自動.js
@@ -37,13 +37,12 @@
 		case 'https://secure.goldpoint.co.jp':
 			switch (pathname) {
 				case '/gpm/membertop.html': {
-					const link = qs('a[href*="/web_meisai/"]')
-					link && link.click()
+					qs('a[href*="/web_meisai/"]').click()
 					break
 				}
 				case '/memx/web_meisai/top/index.html': {
 					const link = qs('.btn_big_size')
-					if (!link || !/\bCSV\b/.test(link.textContent)) break
+					if (!/\bCSV\b/.test(link.textContent)) break
 					link.click()
 					break
 				}
@@ -53,7 +52,6 @@
 			switch (pathname) {
 				case '/auth/realms/msweb/protocol/openid-connect/auth': {
 					const input = qs('input#username')
-					if (!input) break
 					input.autocomplete = 'username'
 					input.focus()
 					break
