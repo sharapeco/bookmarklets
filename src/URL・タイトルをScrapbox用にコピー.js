ae0e04ebc29375863be7d6de045a2a906be6a6ff
Shinichiro Yabu <scin.ichi@gmail.com>
2024-10-30 10:41:21 +0900

    🫧🧼 UTMパラメータを除去するようにした

--- src/URL・タイトルをScrapbox用にコピー.js
+++ src/URL・タイトルをScrapbox用にコピー.js
@@ -1,45 +1,55 @@
-// <div> 経由でコピーする
-(value => {
-	// get <div>
-	((doc, wrap, textarea, label) => {
-		wrap = doc.createElement('div')
-		textarea = doc.createElement('div')
-		Object.assign(textarea.style, {
-			fontSize: '18px',
-			whiteSpace: 'pre-line'
-		})
-		label = doc.createElement('div')
-		label.textContent = 'Click this area to copy.'
-		Object.assign(label.style, {
-			fontSize: '75%',
-			color: '#777'
-		})
-		Object.assign(wrap.style, {
-			position: 'fixed',
-			zIndex: '99999',
-			left: '10px',
-			top: '10px',
-			width: '400px',
-			padding: '10px',
-			background: '#fff',
-			borderRadius: '5px',
-			boxShadow: '0 2px 5px rgba(0,0,0,20%)',
-			font: '400 16px/1.2 sans-serif',
-			lineHeight: '',
-			cursor: 'pointer'
-		})
-		wrap.appendChild(textarea)
-		wrap.appendChild(label)
-		wrap.onclick = () => {
-			doc.getSelection().selectAllChildren(textarea)
-			doc.execCommand('copy')
-			doc.body.removeChild(wrap)
-		}
-		doc.body.appendChild(wrap)
-		return textarea
-	})(document)
-	.textContent = value
+// クリップボードへのコピーにはユーザの操作が必要なため、
+// <div> を作ってクリックさせています。
+((doc, obj, cleanURL) => {
+	// このブックマークレットを実行した際に、ブラウザのURLを綺麗にしてしまう
+	const newUrl = cleanURL(doc.URL);
+	history.replaceState(history.state, "", newUrl);
+
+	// コピーする文字列
+	const value = `[${doc.title} ${newUrl}]`;
+
+	const wrap = doc.createElement("div");
+	obj.assign(wrap.style, {
+		position: "fixed",
+		zIndex: "99999",
+		left: "10px",
+		top: "10px",
+		width: "400px",
+		margin: "0",
+		padding: "10px",
+		background: "#fff",
+		borderRadius: "5px",
+		boxShadow: "0 2px 5px rgba(0,0,0,20%)",
+		font: "400 16px/1.2 sans-serif",
+		lineHeight: "",
+		fontSize: "18px",
+		whiteSpace: "pre-line",
+		cursor: "pointer",
+	});
+	wrap.textContent = value;
+
+	const label = doc.createElement("div");
+	label.textContent = "Click this area to copy.";
+	obj.assign(label.style, {
+		fontSize: "75%",
+		color: "#aaa",
+	});
+	wrap.append(label);
+
+	wrap.onclick = () => {
+		navigator.clipboard.writeText(value);
+		wrap.remove();
+	};
+	doc.body.append(wrap);
 })(
-	// ここにコピーする文字列
-	`[${document.title} ${document.URL}]`
-)
+	document,
+	Object,
+	// UTMパラメータを除去する
+	(urlString) => {
+		const url = new URL(urlString);
+		["utm_campaign", "utm_medium", "utm_source", "utm_content"].map((key) => {
+			url.searchParams.delete(key);
+		});
+		return url.toString();
+	},
+);
